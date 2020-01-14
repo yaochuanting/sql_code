e2b4c8b2f6037a809c2f3c6c6370ae7715572a81
yaochuanting <yao_ct@163.com>
2020-01-14 14:11:11 +0800

    add a new sql file

---
+++ 不同城市接通率.sql
@@ -0,0 +1,15 @@
+select  c.cityname,
+		count(tcr.student_intention_id) as call_cnt,
+        count(case when tcr.status=33 then tcr.student_intention_id else null end) as bridge_cnt,
+        count(tcr.student_intention_id)/count(case when tcr.status=33 then tcr.student_intention_id else null end) as bridge_rate
+
+from hfjydb.view_tms_call_record tcr
+left join hfjydb.view_student s on s.student_intention_id=tcr.student_intention_id
+left join hfjydb.map_phone_city c on c.phone7=left(s.phone,7)
+inner join bidata.charlie_dept_month_end cdme on cdme.user_id=tcr.user_id
+           and cdme.class='销售' and cdme.stats_date=curdate()
+where date(tcr.start_time)>='2019-09-01' 
+      and date(tcr.start_time) < curdate()
+      and tcr.call_type=1
+      and tcr.student_intention_id is not null 
+group by cityname
